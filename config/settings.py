1e0ac1e92f0ec20bffb8ad14588adf4bf191da73
Rishabh <rishabhdeepakk@gmail.com>
2026-06-15 17:06:43 +0530

    Created the apps Dashboard, Accounts and Portfolio. Updated the required settings as well.

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	
+    'dashboard.apps.DashboardConfig',
+	'accounts.apps.AccountsConfig',
+	'portfolio.apps.PortfolioConfig'
 ]
 
 MIDDLEWARE = [
