7910be67f2bd70b0e53de0e064d9eb358481ac24
Rishabh <rishabhdeepakk@gmail.com>
2026-06-19 18:47:00 +0530

    Fixed a bug in the login view

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import AuthenticationForm
 def login_view(request):
 	if request.method == 'POST':
 		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid:
+		if form.is_valid():
 			user = form.get_user()
 			login(request, user)
 			return redirect('dashboard')
@@ -20,6 +20,8 @@ def register_view(request):
 		form = RegisterUserForm(request.POST)
 		if form.is_valid():
 			form.save()
+			user = form.save()
+			login(request, user)
 			return redirect('dashboard')
 	else:
 		form = RegisterUserForm()
