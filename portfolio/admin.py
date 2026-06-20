4e6fba6d02cc943362818d66af4b826072af733d
Rishabh <rishabhdeepakk@gmail.com>
2026-06-20 14:25:18 +0530

    Customized Admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,77 @@
 from django.contrib import admin
 from .models import Portfolio, Asset, Transaction
 
-admin.site.register(Portfolio)
-admin.site.register(Asset)
-admin.site.register(Transaction)
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'user',
+        'base_currency',
+        'created_at',
+    )
+
+    search_fields = (
+        'name',
+        'user__username',
+    )
+
+    list_filter = (
+        'base_currency',
+        'created_at',
+    )
+
+    ordering = (
+        '-created_at',
+    )
+    
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = (
+        'symbol',
+        'name',
+		'asset_type',
+        'exchange',
+        'sector',
+    )
+
+    search_fields = (
+        'symbol',
+		'name',
+    )
+
+    list_filter = (
+        'asset_type',
+		'exchange',
+    )
+
+    ordering = (
+        'symbol',
+    )
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'portfolio',
+    	'asset',
+    	'transaction_type',
+    	'quantity',
+    	'price',
+    	'transaction_date',
+    )
+
+    search_fields = (
+    'asset__symbol',
+    'asset__name',
+    'portfolio__name',
+	)
+
+    list_filter = (
+    'transaction_type',
+    'transaction_date',
+	)
+
+    ordering = (
+        ('-transaction_date',)
+    )
